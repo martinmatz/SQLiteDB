99b961db6a90cf8a879b18d406701f78a8e8a607
Martin Matz <martin.matz@gmail.com>
2020-04-14 20:30:36 +0200

    reorg project structure

--- Package.swift
+++ Package.swift
@@ -26,8 +26,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SQLiteDB",
-            dependencies: [],
-            path: "./"
+            dependencies: []
             ),
     ]
 )
